f97a00dd695d61ca3c3d4bb499dcff367d71740c
Lee <leea20@wit.edu>
2022-07-24 22:01:04 -0400

    working on main menu

--- main_menu.py
+++ main_menu.py
@@ -11,6 +11,7 @@ def main():
 	print("3. View lunar eclipse dates")
 	print("4. View solar conjuction dates")
 	print("5. View solar system simulation")
+	print("0. Exit")
 	choice = input("Please choose an option from above: ")
 	if choice == "1":
 		#query()
@@ -19,57 +20,58 @@ def main():
 	elif choice == "2":
 		# QUERY FOR ALL SOLAR ECLIPSES
 		print("Entire table of solar eclipses:")
-		cursor.execute("""SELECT * FROM SOLAR_ECLIPSES""")
+		cursor.execute("""SELECT SOLAR_ECLIPSES.DATE FROM SOLAR_ECLIPSES""")
 		query_result = cursor.fetchall()
 		for i in query_result:
-			print(i)
+			print("Date: ", i)
 		print("\n")
 		main()
 	elif choice == "3":
 		# QUERY FOR ALL LUNAR ECLIPSES
 		print("Entire table of lunar eclipses:")
-		cursor.execute("""SELECT * FROM LUNAR_ECLIPSES""")
+		cursor.execute("""SELECT LUNAR_ECLIPSES.DATE FROM LUNAR_ECLIPSES""")
 		query_result = cursor.fetchall()
 		for i in query_result:
-			print(i)
+			print("Date: ", i)
 		print("\n")
 		main()
 	elif choice == "4":
-		# QUERY FOR ALL BODIES
-		print("Entire table of celestial bodies:")
-		cursor.execute("""SELECT * FROM BODIES""")
-		query_result = cursor.fetchall()
-		for i in query_result:
-			print(i)
-		print("\n")
-		# QUERY FOR ALL MOONS
-		print("Entire table of all moons:")
-		cursor.execute("""SELECT * FROM MOON""")
-		query_result = cursor.fetchall()
-		for i in query_result:
-			print(i)
-		print("\n")
+		# # QUERY FOR ALL BODIES
+		# print("Entire table of celestial bodies:")
+		# cursor.execute("""SELECT * FROM BODIES""")
+		# query_result = cursor.fetchall()
+		# for i in query_result:
+		# 	print(i)
+		# print("\n")
+		# # QUERY FOR ALL MOONS
+		# print("Entire table of all moons:")
+		# cursor.execute("""SELECT * FROM MOON""")
+		# query_result = cursor.fetchall()
+		# for i in query_result:
+		# 	print(i)
+		# print("\n")
 		# QUERY FOR ALL CONJUNCTIONS
 		print("Entire table of planet conjunctions:")
-		cursor.execute("""SELECT * FROM CONJUNCTION""")
+		print("1 = Mercury, 2 = Venus, 3 = Earth, 4 = Mars, 5 = Jupiter, 6 = Saturn, 7 = Uranus, 8 = Neptune, 9 = Pluto")
+		cursor.execute("""SELECT CONJUNCTION.DATE, CONJUNCTION.FIRST_PLANET, CONJUNCTION.SECOND_PLANET FROM CONJUNCTION""")
 		query_result = cursor.fetchall()
 		for i in query_result:
 			print(i)
 		print("\n")
-		# QUERY FOR ALL LUNAR ECLIPSES
-		print("Entire table of lunar eclipses:")
-		cursor.execute("""SELECT * FROM LUNAR_ECLIPSES""")
-		query_result = cursor.fetchall()
-		for i in query_result:
-			print(i)
-		print("\n")
-		# QUERY FOR ALL SOLAR ECLIPSES
-		print("Entire table of solar eclipses:")
-		cursor.execute("""SELECT * FROM SOLAR_ECLIPSES""")
-		query_result = cursor.fetchall()
-		for i in query_result:
-			print(i)
-		print("\n")
+		# # QUERY FOR ALL LUNAR ECLIPSES
+		# print("Entire table of lunar eclipses:")
+		# cursor.execute("""SELECT * FROM LUNAR_ECLIPSES""")
+		# query_result = cursor.fetchall()
+		# for i in query_result:
+		# 	print(i)
+		# print("\n")
+		# # QUERY FOR ALL SOLAR ECLIPSES
+		# print("Entire table of solar eclipses:")
+		# cursor.execute("""SELECT * FROM SOLAR_ECLIPSES""")
+		# query_result = cursor.fetchall()
+		# for i in query_result:
+		# 	print(i)
+		# print("\n")
 		main()
 	elif choice == "5":
 		#horizonstesting.py <---Is this the correct .py file for showing the solar system simulation?
@@ -78,6 +80,8 @@ def main():
 		solarSystem.simulation(dateStr)
 
 		main()
+	elif choice == "0":
+		print("Goodbye.")
 	else:
 		print("That is not a valid choice.")
 		main()
